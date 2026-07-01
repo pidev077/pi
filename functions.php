c2c346d8ce0adb387a333032f22bbcab188e2a99
Anh Phi Nguyen <nguyenanhphi077@gmail.com>
2026-07-01 08:49:07 +0700

    d

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ require get_template_directory() . '/inc/initialize.php';
 require get_template_directory() . '/inc/assets.php';
 require get_template_directory() . '/inc/acf-options.php';
 require get_template_directory() . '/inc/post-types.php';
-require get_template_directory() . '/inc/meta.php';
+//require get_template_directory() . '/inc/meta.php';
 require get_template_directory() . '/inc/images.php';
 require get_template_directory() . '/inc/helpers.php';
 require get_template_directory() . '/inc/template-tags.php';
